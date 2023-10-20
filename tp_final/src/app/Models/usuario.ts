a3cfcf567be7c26f1dd8fe502ee586d9114b65af
santirinaldi <santiagorinaldi97@gmail.com>
2023-10-20 00:10:59 -0300

    create register-user component

---
+++ tp_final/src/app/Models/usuario.ts
@@ -0,0 +1,9 @@
+export class Usuario {
+    Nombre: String = '';
+    Apellido: String = '';
+    Email: String = '';
+    Pass: String = '';
+    Estado: Boolean = true;
+    Rutinas: String[] = [];
+    Recetas: String[] = [];
+}
