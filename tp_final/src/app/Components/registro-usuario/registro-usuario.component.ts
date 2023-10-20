558ee77c1ea3764b8ca141f805d0b7db7bc00f42
santirinaldi <santiagorinaldi97@gmail.com>
2023-10-20 01:09:39 -0300

    last changes

--- tp_final/src/app/Components/registro-usuario/registro-usuario.component.ts
+++ tp_final/src/app/Components/registro-usuario/registro-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from '../../Models/usuario';
 
 @Component({
   selector: 'app-registro-usuario',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro-usuario.component.css']
 })
 export class RegistroUsuarioComponent {
+
+  userName: string = '';
+  userLastname: string = '';
+  userEmail: string = '';
+  userPass: string = '';
+
+  constructor() {}
+
+  addUser() {
+    let user = new Usuario();
+    user.Nombre = this.userName;
+    user.Apellido = this.userLastname;
+    user.Email = this.userEmail;
+    user.Pass = this.userPass;
+  }
 
 }
