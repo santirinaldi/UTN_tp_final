69141c094d4fc27342425dc4d54a2e97c4f759ef
JosefinaFructuoso <fructuosojosefina@gmail.com>
2023-10-30 01:02:29 -0300

    editar

--- tp_final/src/app/Components/editar-usuario/editar-usuario.component.ts
+++ tp_final/src/app/Components/editar-usuario/editar-usuario.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef } from '@angular/core';
+import { Usuario } from 'src/app/Models/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-editar-usuario',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./editar-usuario.component.css']
 })
 export class EditarUsuarioComponent {
+  userName: string = '';
+  userLastname: string = '';
+  userEmail: string = '';
+  userPass: string = '';
+
+
+constructor(private servicioUsuario: UsuarioService) {}
+
+  addUser() {
+    let user = new Usuario();
+    user.name = this.userName;
+    user.lastName = this.userLastname;
+    user.email = this.userEmail;
+    user.passWord = this.userPass;
+    this.servicioUsuario.add(user);
+  }
 
 }
