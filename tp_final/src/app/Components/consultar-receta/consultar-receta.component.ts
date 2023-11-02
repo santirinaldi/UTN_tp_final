69c573ca49974d5add0e1875de24183022265299
agus3789 <65677650+agus3789@users.noreply.github.com>
2023-11-02 19:46:51 -0300

    cambios boludos

--- tp_final/src/app/Components/consultar-receta/consultar-receta.component.ts
+++ tp_final/src/app/Components/consultar-receta/consultar-receta.component.ts
@@ -1,4 +1,4 @@
-import { Component,ViewChild,ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { GetAPIService } from 'src/app/services/API/get-api.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { Usuario } from 'src/app/Models/usuario';
@@ -10,26 +10,35 @@ import { JSONService } from 'src/app/services/JSON/json.service';
   styleUrls: ['./consultar-receta.component.css'],
 })
 export class ConsultarRecetaComponent {
-  apiResponse: string = "testtesttesttesttesttest";
-  @ViewChild('result')result!:ElementRef;
+  apiResponse: string = 'testtesttesttesttesttest';
+  @ViewChild('result') result!: ElementRef;
 
   food: Array<string> = [];
   objetives: Array<string> = [];
   foodLimits: Array<string> = [];
   maxCalories: number = 0;
 
-  constructor(private apiservice: GetAPIService, private servicioUsuario: UsuarioService, private servicioJson: JSONService) {}
+  constructor(
+    private apiservice: GetAPIService,
+    private servicioUsuario: UsuarioService,
+    private servicioJson: JSONService
+  ) { }
 
   createMessage() {
-    console.log(this.food.toString());
-    const message = ` Quiero una receta con estas caracteristicas: Tipo de comida: ${this.food.toString()} . Objetivos: ${this.objetives.toString()}. Calorias Maximas: ${this.maxCalories}. Limitaciones: ${this.foodLimits.toString()}.`;
-    console.log(message);
-    //this.pedidoAPI(message);
-    this.createView();
+    const log = this.servicioUsuario.checkLoggedIn();
+    if (log !== null) {
+      console.log(this.food.toString());
+      const message = ` Quiero una receta con estas caracteristicas: Tipo de comida: ${this.food.toString()} . Objetivos: ${this.objetives.toString()}. Calorias Maximas: ${this.maxCalories
+        }. Limitaciones: ${this.foodLimits.toString()}.`;
+      console.log(message);
+      //this.pedidoAPI(message);
+      this.createView(Number(log));
+    }
   }
 
   pedidoAPI(message: string) {
-    this.apiservice.apiRequest(message)
+    this.apiservice
+      .apiRequest(message)
       .then((response) => response.json())
       .then((json) => {
         //this.apiResponse = json.answer;
@@ -38,28 +47,21 @@ export class ConsultarRecetaComponent {
       .catch((error) => console.log(error));
   }
 
-  agregarReceta() {
-    const log = this.servicioUsuario.checkLoggedIn();
-    if(log !== null) {
-      const user:Usuario = this.servicioUsuario.getUser(Number(log));
+  agregarReceta(log:number) {
+      const user: Usuario = this.servicioUsuario.getUser(log);
       user.listaRecetas.push(this.apiResponse);
       this.servicioJson.putUser(user);
-    } 
   }
 
-  createView() {
-    const p = document.createElement("p");
-    const btnAdd = document.createElement("button");
-    btnAdd.textContent = "Agregar a mi Lista";
+  createView(log:number) {
+    const p = document.createElement('p');
+    const btnAdd = document.createElement('button');
+    btnAdd.textContent = 'Agregar a mi Lista';
     btnAdd.onclick = () => {
-      this.agregarReceta();
-    }
+      this.agregarReceta(log);
+    };
     p.textContent = this.apiResponse;
     this.result.nativeElement.appendChild(p);
     this.result.nativeElement.appendChild(btnAdd);
   }
 }
-
-
-
-
