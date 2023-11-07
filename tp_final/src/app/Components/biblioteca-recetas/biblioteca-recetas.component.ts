6b295192d3658d91013f553a48d0a78ae3af7b66
JosefinaFructuoso <fructuosojosefina@gmail.com>
2023-11-06 23:12:01 -0300

    biblioteca recetas

--- tp_final/src/app/Components/biblioteca-recetas/biblioteca-recetas.component.ts
+++ tp_final/src/app/Components/biblioteca-recetas/biblioteca-recetas.component.ts
@@ -2,6 +2,7 @@ import { Component,ViewChild,ElementRef,OnInit } from '@angular/core';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { Usuario } from 'src/app/Models/usuario';
 
+
 @Component({
   selector: 'app-biblioteca-recetas',
   templateUrl: './biblioteca-recetas.component.html',
@@ -14,22 +15,12 @@ export class BibliotecaRecetasComponent {
   @ViewChild('lista')lista!:ElementRef;
   @ViewChild('popupItem')popupItem!:ElementRef;
 
-  crearView() {
+   
     const log = this.servicioUsuario.checkLoggedIn();
-    const userList = this.servicioUsuario.getUsers();
-    console.log(userList);
-    userList.forEach((item) => {
-      console.log(item);
-    })
-  //   user.bibliotecaRecetas.listaRecetas.forEach((receta) => {
-  //     const li = document.createElement('li');
-  //     const btnRemove = document.createElement('button');
-  //     const p = document.createElement('p');
-  //     p.textContent = String(receta.texto);
-  //     li.appendChild(p);
-  //     this.lista.nativeElement.appendChild(li);
-  //   })
-  // }
-  }
+
+    const user: Usuario | undefined; 
+    this.servicioUsuario.getUser(1).suscribe((usuario: Usuario)=>this.user=usuario);
+    
+
   
 }
