c0cebc06b0bda3a3a7062de73e30e0b25018be82
agus3789 <65677650+agus3789@users.noreply.github.com>
2023-11-14 20:58:45 -0300

    biblioteca-recetas + session

--- tp_final/src/app/Components/biblioteca-recetas/biblioteca-recetas.component.ts
+++ tp_final/src/app/Components/biblioteca-recetas/biblioteca-recetas.component.ts
@@ -1,20 +1,40 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { Usuario } from 'src/app/Models/usuario';
+import { LoginService } from 'src/app/services/auth/login.service';
+import { JSONService } from 'src/app/services/JSON/json.service';
 
 @Component({
   selector: 'app-biblioteca-recetas',
   templateUrl: './biblioteca-recetas.component.html',
   styleUrls: ['./biblioteca-recetas.component.css'],
 })
-export class BibliotecaRecetasComponent {
+export class BibliotecaRecetasComponent implements OnInit {
   loggedInStatus!: Number;
   userLogged!: Usuario;
 
-  constructor(private servicioUsuario: UsuarioService) {}
+  constructor(
+    private servicioUsuario: UsuarioService,
+    private loginService: LoginService,
+    private jsonService: JSONService
+  ) {}
 
   @ViewChild('lista') lista!: ElementRef;
   @ViewChild('popupItem') popupItem!: ElementRef;
+
+  ngOnInit(): void {
+    this.loginService.getisLoggedIn().subscribe((value) => {
+      this.loggedInStatus = value;
+      if (this.loggedInStatus != -1) {
+        this.jsonService.getUserByID(this.loggedInStatus).subscribe((user) => {
+          this.userLogged = user;
+          console.log(this.userLogged);
+        });
+      } else {
+        console.log('nada');
+      }
+    });
+  }
 
   // crearView() {
   //   const log = this.servicioUsuario.checkLoggedIn();
