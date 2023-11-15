f739b2e1156f8e719198bd2f37bd5ace5ae15a3f
agus3789 <65677650+agus3789@users.noreply.github.com>
2023-11-14 21:00:39 -0300

    biblioteca-rutinas + session

--- tp_final/src/app/Components/biblioteca-rutinas/biblioteca-rutinas.component.ts
+++ tp_final/src/app/Components/biblioteca-rutinas/biblioteca-rutinas.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/auth/login.service';
+import { JSONService } from 'src/app/services/JSON/json.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { Usuario } from 'src/app/Models/usuario';
 
 @Component({
   selector: 'app-biblioteca-rutinas',
   templateUrl: './biblioteca-rutinas.component.html',
-  styleUrls: ['./biblioteca-rutinas.component.css']
+  styleUrls: ['./biblioteca-rutinas.component.css'],
 })
-export class BibliotecaRutinasComponent {
+export class BibliotecaRutinasComponent implements OnInit {
+  loggedInStatus!: Number;
+  userLogged!: Usuario;
 
+  constructor(
+    private servicioUsuario: UsuarioService,
+    private loginService: LoginService,
+    private jsonService: JSONService
+  ) {}
+
+  ngOnInit(): void {
+    // this.isLogged = this.servicioUsuario.checkLoggedIn();
+
+    this.loginService.getisLoggedIn().subscribe((value) => {
+      this.loggedInStatus = value;
+      if (this.loggedInStatus != -1) {
+        this.jsonService.getUserByID(this.loggedInStatus).subscribe((user) => {
+          this.userLogged = user;
+          console.log(this.userLogged);
+        });
+      } else {
+        console.log('nada');
+      }
+    });
+  }
 }
-
