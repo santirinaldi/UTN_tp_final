371f40acea43d986a9d1510e3889e9d10de5413f
JosefinaFructuoso <fructuosojosefina@gmail.com>
2023-11-20 12:07:05 -0300

    ultimos cambios

--- tp_final/src/app/Components/biblioteca-recetas/biblioteca-recetas.component.ts
+++ tp_final/src/app/Components/biblioteca-recetas/biblioteca-recetas.component.ts
@@ -64,6 +64,9 @@ export class BibliotecaRecetasComponent implements OnInit {
     this.mostrarVer=!this.mostrarVer;
   }
 
+  /*  <div *ngIf="mostrarVer"
+  (click)="modificarMostrarVer()"*/
+
 
   nombreListaActualizar(nombre: string) {
     const usuarioActualizado: Usuario = {
