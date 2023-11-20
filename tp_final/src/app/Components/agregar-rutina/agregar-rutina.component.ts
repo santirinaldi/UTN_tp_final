07d1f92cc3c84633164c4a832c5de3625eb441cd
santirinaldi <santiagorinaldi97@gmail.com>
2023-11-20 13:14:55 -0300

    l c

--- tp_final/src/app/Components/agregar-rutina/agregar-rutina.component.ts
+++ tp_final/src/app/Components/agregar-rutina/agregar-rutina.component.ts
@@ -90,7 +90,12 @@ export class AgregarRutinaComponent implements OnInit {
         this.equipment
       }. ${this.preferences}`;
       
-      this.createView();
+
+      this.apiservice._apiRequest(message).subscribe((response) => {
+        console.log(response);
+        this.apiResponse = response.answer;
+        this.createView();
+      });
 
     }else{
 
